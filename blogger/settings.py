14fe1ba8b52378e22475664e5f3021c2a130ee21
Ferdileadguitar <ferdiardiansa@rocketmail.com>
2017-09-14 00:50:08 +0700

    mysql conf

--- blogger/settings.py
+++ blogger/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'blogger.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite2',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite2'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE'    : 'django.db.backends.mysql',
+        'NAME'      : 'blogger',
+        'USER'      : 'root',
+        'PASSWORD'  : 'root',
+        'HOST'      : 'localhost',
+        'PORT'      : ''
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
